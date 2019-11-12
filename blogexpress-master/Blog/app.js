7f3af0caada90c53001af9d0ecc8ce03efd228ed
wujinya2017 <2601519403@qq.com>
2019-11-12 16:14:55 +0800

    zuoye 5

---
+++ blogexpress-master/Blog/app.js
@@ -0,0 +1,27 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+app.use('/', indexRouter);
+app.use('/login', indexRouter);
+app.use('/home', indexRouter);
+app.use(cookieParser());
+
+module.exports = app;
---
+++ blogexpress-master/Blog/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const path =require('./data.json');
+
+var user = path.users;
+var chapterList = path.chapterList;
+router.get('/', function(req, res, next) {
+    res.render('index', { title: '登录' });
+});
+router.post('/home',function(req,res){ 
+    if(req.body.username===user[0].username&&req.body.pwd===user[0].password){
+        res.render('home',{title:'home'})
+    }else{
+        res.send('用户名或密码错误，请重新输入');
+    }
+});
+
+router.get('/home', function(req, res,next) {
+    res.render('home', { title: '详情' });
+});
+
+router.get('/add',function(req,res){
+    var bb = JSON.stringify(chapterList);
+    res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(bb),
+        'Content-Type': 'text/plain;charset="utf-8"'
+    })
+  res.end(bb)
+})
+
+module.exports = router;
+
+
+
